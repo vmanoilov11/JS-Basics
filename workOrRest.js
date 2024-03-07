f274799d84a6ecdf2843c6e5d6a7dfb9a44ea1e4
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-07 23:29:43 +0200

    Add files via upload

---
+++ workOrRest.js
@@ -0,0 +1,25 @@
+function days(input) {
+
+    let day = input[0]
+
+    switch (day) {
+
+        case "Monday":
+        case "Tuesday":
+        case "Wednesday":
+        case "Thursday":
+        case "Friday":
+            console.log("Working day")
+                ; break
+        case "Saturday":
+        case "Sunday":
+            console.log("Weekend")
+            ;break
+
+        default:
+            console.log("Error")
+
+
+    }
+}
+days(["Sunday"])
