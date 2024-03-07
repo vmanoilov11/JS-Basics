7ed6786dbc2cd9e57cdd0c0ae1ed5a9f8cfc5118
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-07 23:27:30 +0200

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,23 @@
+function nestedConditionals() {
+
+    let pass = "Vlado1312"
+
+    switch (pass) {
+
+
+        case "Hi123": console.log("Hi user!");
+         break
+        case "PBJS2021": console.log("Js is the best!"); 
+        break
+        case "Vlado1312": console.log("Cool!");
+         break
+        case "wiund": console.log("Ne");
+         break
+        default: console.log("Error");
+         break
+
+
+    }
+
+}
+nestedConditionals()
