f5e0a7229700d5782a9f16e88129512a8c80339c
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-07 23:27:07 +0200

    Add files via upload

---
+++ cinema.js
@@ -0,0 +1,22 @@
+function cinema(input) {
+
+    let day = input[0]
+
+    if (day === "Monday") {
+        console.log(12)
+    } else if (day === "Tuesday") {
+        console.log(12)
+    } else if (day === "Wednesday") {
+        console.log(14)
+    } else if (day === "Thursday") {
+        console.log(14)
+    } else if (day === "Friday") {
+        console.log(12)
+    } else if (day === "Saturday") {
+        console.log(16)
+    } else if (day === "Sunday") {
+        console.log(16)
+    }
+
+}
+cinema(["Sunday"])
