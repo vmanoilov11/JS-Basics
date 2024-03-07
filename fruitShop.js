5826ea26cd4d30b0a1efe7e67ba9b549287054ea
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-07 23:28:05 +0200

    Add files via upload

---
+++ fruitShop.js
@@ -0,0 +1,14 @@
+function fruit(input){
+
+    let fruit = input[0]
+    let day = input[1]
+    let amount = Number(input[2])
+
+    switch(day){
+        case "Monday": 
+        case "Tuesday":
+        case "Wednesday":
+        case "Thursday":
+        case "Friday":
+    }
+}
