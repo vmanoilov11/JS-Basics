83f22fc79016c061e15cfaf356160bc0297b8263
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-07 23:26:42 +0200

    Add files via upload

--- boat.js
+++ boat.js
@@ -1,45 +1,55 @@
 function boat(input) {
 
-    let budget = Number(input[0])
-    let season = input[1]
-    let fishermans = Number(input[2])
+    let index = 0
 
-    let price = 0
+    let budget = Number(input[index]);
+    index++
+    let season = input[1];
+    index++
+    let fishermans = Number(input[index]);
+    index++
 
-    switch (season) {
+    let price = 0;
+
+            switch (season) {
         case "Spring":
-            price = 3000
-
-            ; break
+            price = 3000; break;
         case "Summer":
         case "Autumn":
-            price = 4200
-            ; break
+            price = 4200; break;
         case "Winter":
-            price = 2600
-            ; break
-    }
+            price = 2600; break;
 
-    if (fishermans <= 6) {
-        price = price * 0.90
-    } else if (fishermans >= 7 && fishermans <= 11) {
+
+
+        } if(fishermans <= 6){
+        price = price  * 0.90
+
+        }else if(fishermans >= 7 && fishermans <=11){
         price = price * 0.85
-    } else {
+
+        }else if(fishermans >= 12){
         price = price * 0.75
     }
-    if (fishermans % 2 && season !== "Autumn") {
+
+        if(fishermans % 2 == 0 && season !== "Autumn"){
         price = price * 0.95
+        }
 
-    }
-    let diff = Math.abs(budget - price)
+        let diff = Math.abs(budget - price)
 
-    if (budget >= price) {
-        console.log(`Yes! You have ${diff.toFixed(2)} leva left.`)
-    } else {
-        console.log(`Not enough money! You need ${diff.toFixed(2)} leva.`)
-    }
-
-
-
+        if(budget >= price){
+            console.log(`Yes! You have ${diff.toFixed(2)} leva left.`)
+        }else{
+            console.log(`Not enough money! You need ${diff.toFixed(2)} leva.`)
+        }
 }
-boat(["3000", "Summer", "11"])
+boat(["2000",
+"Winter",
+"13"])
+
+
+
+
+
+
