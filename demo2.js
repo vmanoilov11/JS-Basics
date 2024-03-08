90c72cf99cc22fd9c2f9118d33870a86dc7e6537
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-08 23:06:08 +0200

    Add files via upload

---
+++ demo2.js
@@ -0,0 +1,13 @@
+function whileDemo(input){
+
+    let totalSum = 0
+    let index = 0
+    let currentInput = input[index]
+
+    while(currentNum !== "NoMoreMoney"){
+        let inputAsNumber = Number(currentInput)
+        if(currentNum < 0){
+            
+        }
+    }
+}
