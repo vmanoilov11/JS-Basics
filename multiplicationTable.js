aba1fabce1d0542f0b75605abb70e8e7972b1f3b
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-08 23:06:34 +0200

    Add files via upload

---
+++ multiplicationTable.js
@@ -0,0 +1,11 @@
+function solve(input) {
+
+    let secNum = Number(input[0]);
+
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${i} * ${secNum} = ${i * secNum}`)
+
+
+    }
+}
+solve(["5"])
