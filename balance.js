5c987e07bc54f5e86f3ef54fad3d262144d72f2e
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-09 14:18:40 +0200

    Add files via upload

---
+++ balance.js
@@ -0,0 +1,26 @@
+function accBalance(input) {
+
+    let deposit = Number(input[0])
+    let balance = 0
+    let index = 1
+
+    while (deposit !== "NoMoreMoney") {
+        let amount = Number(deposit)
+        if (amount < 0) {
+            console.log("Invalid operation!"); break
+        }
+        balance += amount
+        console.log(`Increase: ${amount.toFixed(2)}`)
+        deposit = input[index]
+        index++
+
+
+
+    }
+    console.log(`Total: ${balance.toFixed(2)}`)
+}
+accBalance
+    (["5.51",
+        "69.42",
+        "100",
+        "NoMoreMoney"])
